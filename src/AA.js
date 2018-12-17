d0c722ab34f443856f87700e0f94d359df789b03
深蓝 <979668507@qq.com>
2018-12-17 22:50:23 +0800

    添加redux

--- src/AA.js
+++ src/AA.js
@@ -1,15 +1,9 @@
-"use strict";
-import React ,{Component} from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import Home from './Home'
-import About from './About'
-import Time from './Time'
-import Topics from './Topics'
-import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+/**
+ * Created by ren on 2018/12/17.
+ */
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-
+import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import { Provider, connect } from 'react-redux'
 
@@ -67,14 +61,4 @@ const AA = connect(
     mapStateToProps,
     mapDispatchToProps
 )(Counter)
-ReactDOM.render(
-    <Provider store={store}>
-        <AA />
-    </Provider>
-    ,
-    document.getElementById('root')
-);
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export default AA;
